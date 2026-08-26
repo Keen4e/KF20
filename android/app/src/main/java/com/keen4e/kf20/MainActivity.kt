6effd6996f6976c37638e36365caed4390dff7d7
Keen4e <121856608+Keen4e@users.noreply.github.com>
2026-08-26 21:28:50 +0200

    Restore Android version import

--- android/app/src/main/java/com/keen4e/kf20/MainActivity.kt
+++ android/app/src/main/java/com/keen4e/kf20/MainActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.provider.OpenableColumns
 import android.os.Bundle
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.speech.RecognizerIntent
