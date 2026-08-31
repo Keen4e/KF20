7ebaad5ead0c2627b02331fe36e42dfd51feb135
Keen4e <121856608+Keen4e@users.noreply.github.com>
2026-08-31 13:38:29 +0200

    Use Compose rule node lookup

--- android/app/src/androidTest/java/com/keen4e/kf20/MainActivityFlowTest.kt
+++ android/app/src/androidTest/java/com/keen4e/kf20/MainActivityFlowTest.kt
@@ -3,7 +3,6 @@ package com.keen4e.kf20
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
